cca5cae147a1bf5083041a3109a48fc8670c6c68
Murali <muraligiri.mk@gmail.com>
2023-01-26 11:57:43 +0530

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -27,7 +27,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt,{ secret: secret, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
