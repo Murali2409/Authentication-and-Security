d787c4904cdadbea333eb6ba84f40be4163f9c52
Murali <muraligiri.mk@gmail.com>
2023-01-26 12:41:48 +0530

    With all files sub level -2

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -27,8 +28,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt,{ secret: secret, encryptedFields:["password"] });
+userSchema.plugin(encrypt,{ secret: process.env.SECRET, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
